cc7550c7b2a9ad8f11c2047e4842994ebecffc7a
Hasan Alp Zengin <hasan_alp.zengin@mercedes-benz.com>
2023-05-15 15:52:35 +0200

    change list matcher to match any item in the list

--- mapmatcher/src/main/java/io/github/nik9000/mapmatcher/ListMatcher.java
+++ mapmatcher/src/main/java/io/github/nik9000/mapmatcher/ListMatcher.java
@@ -28,146 +28,149 @@ import org.hamcrest.TypeSafeMatcher;
  * Matcher for {@link List Lists} that reports all errors at once.
  */
 public class ListMatcher extends TypeSafeMatcher<List<?>> {
-  /**
-   * Create a {@linkplain ListMatcher} that matches empty lists.
-   */
-  public static ListMatcher matchesList() {
-    return new ListMatcher(emptyList());
-  }
-
-  /**
-   * Create a {@linkplain ListMatcher} that matches a list.
-   */
-  public static ListMatcher matchesList(List<?> list) {
-    ListMatcher matcher = matchesList();
-    for (Object item : list) {
-      matcher = matcher.item(item, false);
+    /**
+     * Create a {@linkplain ListMatcher} that matches empty lists.
+     */
+    public static ListMatcher matchesList() {
+        return new ListMatcher(emptyList());
     }
-    return matcher;
-  }
 
-  /*
-  * Create a {@linkplain ListMatcher} that matches a list
-  * with extraOk param which will applied to the children
-  */
-  public static ListMatcher matchesList(List<?> list, Boolean extraOkForChildrenMap) {
-    ListMatcher matcher = matchesList();
-    for (Object item : list) {
-      matcher = matcher.item(item, extraOkForChildrenMap);
+    /**
+     * Create a {@linkplain ListMatcher} that matches a list.
+     */
+    public static ListMatcher matchesList(List<?> list) {
+        ListMatcher matcher = matchesList();
+        for (Object item : list) {
+            matcher = matcher.item(item, false);
+        }
+        return matcher;
     }
-    return matcher;
-  }
 
-  private final List<Matcher<?>> matchers;
-
-  private ListMatcher(List<Matcher<?>> matchers) {
-    this.matchers = matchers;
-  }
-
-
-  /**
-   * Expect a value.
-   * <p>
-   * Passing a {@link Matcher} to this method will function as though you
-   * passed it directly to {@link #item(Matcher)}.
-   *
-   * @return a new {@link ListMatcher} that expects all items this matcher
-   *         expected followed by the provided item
-   */
-  public ListMatcher item(Object value) {
-    return item(matcherFor(value, false));
-  }
-
-  /*
-  * Apply the extraOk to all children
-  * */
-  public ListMatcher item(Object value, Boolean extraOk) {
-    return item(matcherFor(value, extraOk));
-  }
-
-  /**
-   * Expect a {@link Matcher}.
-   *
-   * @return a new {@link ListMatcher} that expects all items this matcher
-   *         expected followed by the provided item
-   */
-  public ListMatcher item(Matcher<?> valueMatcher) {
-    if (valueMatcher == null) {
-      valueMatcher = nullValue();
+    /*
+     * Create a {@linkplain ListMatcher} that matches a list
+     * with extraOk param which will applied to the children
+     */
+    public static ListMatcher matchesList(List<?> list, Boolean extraOkForChildrenMap) {
+        ListMatcher matcher = matchesList();
+        for (Object item : list) {
+            matcher = matcher.item(item, extraOkForChildrenMap);
+        }
+        return matcher;
     }
-    List<Matcher<?>> matchers = new ArrayList<>(this.matchers);
-    matchers.add(valueMatcher);
-    return new ListMatcher(matchers);
-  }
 
-  /**
-   * {@inheritDoc}
-   *
-   * @hidden
-   */
-  @Override
-  public void describeTo(Description description) {
-    describeTo(keyWidth(emptyList()), description);
-  }
+    private final List<Matcher<?>> matchers;
 
-  int keyWidth(List<?> item) {
-    int max = Integer.toString(matchers.size()).length();
-    Iterator<?> value = item.iterator();
-    Iterator<Matcher<?>> matcher = matchers.iterator();
-    while (matcher.hasNext()) {
-      max = Math.max(max,
-          maxKeyWidthForMatcher(value.hasNext() ? value.next() : null, matcher.next()));
+    private ListMatcher(List<Matcher<?>> matchers) {
+        this.matchers = matchers;
     }
-    return max;
-  }
 
-  void describeTo(int keyWidth, Description description) {
-    description.appendText(matchers.isEmpty() ? "an empty list" : "a list containing");
-    int index = 0;
-    for (Matcher<?> matcher : matchers) {
-      describeMatcher(keyWidth, index++, matcher, description);
-    }
-  }
 
-  @Override
-  protected boolean matchesSafely(List<?> item) {
-    if (item.size() != matchers.size()) {
-      return false;
+    /**
+     * Expect a value.
+     * <p>
+     * Passing a {@link Matcher} to this method will function as though you
+     * passed it directly to {@link #item(Matcher)}.
+     *
+     * @return a new {@link ListMatcher} that expects all items this matcher
+     * expected followed by the provided item
+     */
+    public ListMatcher item(Object value) {
+        return item(matcherFor(value, false));
     }
-    Iterator<?> value = item.iterator();
-    Iterator<Matcher<?>> matcher = matchers.iterator();
-    while (matcher.hasNext()) {
-      if (false == matcher.next().matches(value.next())) {
-        return false;
-      }
-    }
-    return true;
-  }
 
-  @Override
-  protected void describeMismatchSafely(List<?> item, Description description) {
-    describePotentialMismatch(keyWidth(item), item, description);
-  }
-
-  void describePotentialMismatch(int keyWidth, List<?> item, Description description) {
-    description.appendText(matchers.isEmpty() ? "an empty list" : "a list containing");
-    int maxKeyWidth = Integer.toString(Math.max(item.size(), matchers.size())).length();
-    String keyFormat = "%" + maxKeyWidth + "s";
-
-    Iterator<?> value = item.iterator();
-    Iterator<Matcher<?>> matcher = matchers.iterator();
-    int index = 0;
-    while (matcher.hasNext()) {
-      describeEntry(keyWidth, String.format(Locale.ROOT, keyFormat, index++), description);
-      if (false == value.hasNext()) {
-        describeEntryMissing(matcher.next(), description);
-        continue;
-      }
-      describeEntryValue(keyWidth, matcher.next(), value.next(), description);
+    /*
+     * Apply the extraOk to all children
+     * */
+    public ListMatcher item(Object value, Boolean extraOk) {
+        return item(matcherFor(value, extraOk));
     }
-    while (value.hasNext()) {
-      describeEntry(keyWidth, String.format(Locale.ROOT, keyFormat, index++), description);
-      describeEntryUnexepected(value.next(), description);
+
+    /**
+     * Expect a {@link Matcher}.
+     *
+     * @return a new {@link ListMatcher} that expects all items this matcher
+     * expected followed by the provided item
+     */
+    public ListMatcher item(Matcher<?> valueMatcher) {
+        if (valueMatcher == null) {
+            valueMatcher = nullValue();
+        }
+        List<Matcher<?>> matchers = new ArrayList<>(this.matchers);
+        matchers.add(valueMatcher);
+        return new ListMatcher(matchers);
     }
-  }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @hidden
+     */
+    @Override
+    public void describeTo(Description description) {
+        describeTo(keyWidth(emptyList()), description);
+    }
+
+    int keyWidth(List<?> item) {
+        int max = Integer.toString(matchers.size()).length();
+        Iterator<?> value = item.iterator();
+        Iterator<Matcher<?>> matcher = matchers.iterator();
+        while (matcher.hasNext()) {
+            max = Math.max(max,
+                    maxKeyWidthForMatcher(value.hasNext() ? value.next() : null, matcher.next()));
+        }
+        return max;
+    }
+
+    void describeTo(int keyWidth, Description description) {
+        description.appendText(matchers.isEmpty() ? "an empty list" : "a list containing");
+        int index = 0;
+        for (Matcher<?> matcher : matchers) {
+            describeMatcher(keyWidth, index++, matcher, description);
+        }
+    }
+
+    @Override
+    protected boolean matchesSafely(List<?> item) {
+        Iterator<?> value = item.iterator();
+        for(Matcher<?> matcher: matchers){
+            boolean matching = false;
+            while(value.hasNext()){
+                if(matcher.matches(value)){
+                    matching = true;
+                }
+            }
+            // if any matcher doesn't match with any value of item
+            if(!matching){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    protected void describeMismatchSafely(List<?> item, Description description) {
+        describePotentialMismatch(keyWidth(item), item, description);
+    }
+
+    void describePotentialMismatch(int keyWidth, List<?> item, Description description) {
+        description.appendText(matchers.isEmpty() ? "an empty list" : "a list containing");
+        int maxKeyWidth = Integer.toString(Math.max(item.size(), matchers.size())).length();
+        String keyFormat = "%" + maxKeyWidth + "s";
+
+        Iterator<?> value = item.iterator();
+        Iterator<Matcher<?>> matcher = matchers.iterator();
+        int index = 0;
+        while (matcher.hasNext()) {
+            describeEntry(keyWidth, String.format(Locale.ROOT, keyFormat, index++), description);
+            if (false == value.hasNext()) {
+                describeEntryMissing(matcher.next(), description);
+                continue;
+            }
+            describeEntryValue(keyWidth, matcher.next(), value.next(), description);
+        }
+        while (value.hasNext()) {
+            describeEntry(keyWidth, String.format(Locale.ROOT, keyFormat, index++), description);
+            describeEntryUnexepected(value.next(), description);
+        }
+    }
 }
